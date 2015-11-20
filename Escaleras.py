e4969354d979631c458449c8de66fd64b8ba4488
jer321 <cuentas19971@hotmail.com>
2015-11-20 12:32:38 -0500

    Ahora si

---
+++ Escaleras.py
@@ -0,0 +1,13 @@
+#Escaleras.py
+import pygame as pig
+from Constantes import *
+
+escaleraImg=pig.image.load('Escalera.png')
+escaleras=\
+(790,200,820,240),\
+(140,290,170,330),\
+(310,280,340,340),\
+(790,380,820,420),\
+(480,370,510,440),\
+(140,465,170,510),\
+(410,460,450,520),\
